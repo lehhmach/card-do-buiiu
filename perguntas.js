7029f2759ec349d008907e0192e165602b802500
lehhmach <leticia.silveira.machado@escola.pr.gov.br>
2025-06-12 17:00:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    '1 Matematica',
+     'qual a fórmula para calcular a área do quadrado?',
+    'base x altura'
+)
+
+criaCartao(
+    '2 Biologia',
+    'qual animal pertence aos anelídeos?',
+    'sanguessuga'
+)
+
+criaCartao(
+    '3 Geografia',
+    'Qual é a capital do Paraná?',
+    'Curitiba'
+)
+
+criaCartao(
+    '4 Astronimia',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
